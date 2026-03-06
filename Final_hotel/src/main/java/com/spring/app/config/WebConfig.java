8836afe54cec5815bbe2e8bc4da9b9b7a1437d76
leehk9311 <lhk9311@gmail.com>
2026-03-06 16:11:59 +0900

    roomtype 이미지 경로 추가(배포 고려)

--- Final_hotel/src/main/java/com/spring/app/config/WebConfig.java
+++ Final_hotel/src/main/java/com/spring/app/config/WebConfig.java
@@ -16,12 +16,14 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		
-		registry.addResourceHandler("/file_images/**")
-		.addResourceLocations("file:./file_images/");
+		 registry.addResourceHandler("/file_images/**")
+         .addResourceLocations("file:" + imagesDir + "/");
 		// Spring 은 다음의 순서대로 찾는다. 
 		// 제일먼저, 외부 업로드 폴더를 먼저 검색하고(file_images/쉐보레.jpg) 있으면 이것을 사용하고,
 		// 만약에 없으면 static 을 검색한다.(static/images/쉐보레.jpg)
 		// 그리고 스프링시큐리티 설정파일인 com.spring.app.security.config.SecurityConfig 에서 excludeUri 에 "/images/**" 을 추가해 주어야 한다. 
+		
+		
 		
 	}
 }
