71ffc6d69682b66aa319e5c45609bb94d0b8b702
Jsblow001 <jiseop1203@gmail.com>
2026-03-09 16:46:32 +0900

    인덱스 검색부분 구현

--- Final_hotel/src/main/java/com/spring/app/js/index/service/IndexService.java
+++ Final_hotel/src/main/java/com/spring/app/js/index/service/IndexService.java
@@ -6,17 +6,24 @@ import com.spring.app.js.banner.domain.BannerDTO;
 import com.spring.app.js.promotion.domain.PromotionDTO;
 
 import java.util.List;
+import java.util.Map;
 
 public interface IndexService {
     // 메인 배너 리스트 조회
     List<BannerDTO> getMainBannerList();
 
-    // 메인용 객실 리스트 (최상위 2개)
+    // 메인용 객실 리스트
     List<RoomTypeDTO> getMainRoomList();
 
-    // 메인용 다이닝 리스트 (최상위 3개)
+    // 메인용 다이닝 리스트
     List<DiningDTO> getMainDiningList();
 
     // 메인용 프로모션 리스트
     List<PromotionDTO> getPromoCardList();
+
+    // 객실 검색
+	List<RoomTypeDTO> getAvailableRooms(Map<String, Object> paraMap);
+
+	// 호텔 리스트 
+	List<Map<String, String>> getHotelList();
 }
